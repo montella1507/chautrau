80a569e8c12510a2d13fdb0e17d3ee683d27d748
montella1507 <marian.bencat@live.com>
2022-12-04 10:39:57 +0100

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,8 +10,10 @@ import { AppComponent } from './app.component';
   imports:      [ BrowserModule, FormsModule,
    RouterModule.forRoot([
      {
-       path: "/cars",
-       loadChildren: ()=> import("./data/data.module").then(x=> x.DataModule)
+       path: "cars",
+       loadChildren: ()=> import("./data/data.module").then(x=> x.DataModule.configure({
+         name: "cars"
+       }).ngModule as any)
      },
      {
        path: '',
